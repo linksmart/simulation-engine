b27d9f7dc6c36dec0ba72e7529e7fca18aa67825
Gustavo Aragon <gustavo.aragon@fit.fraunhofer.de>
2018-08-22 13:27:35 +0200

    API main

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python3
 
 import connexion
-import logging
 
 from swagger_server import encoder
 
 
-def create_app():
-    logging.getLogger('connexion.operation').setLevel('ERROR')
+def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'DSF-SE API'})
-    return app
-
-def main():
-    #application=create_app()
-    create_app().run(port=9090)
+    app.run(port=9090)
 
 
 if __name__ == '__main__':
-    create_app()
-    #main()
+    main()
