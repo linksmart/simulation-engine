c3b02e9be49c88ce313f1eaf5debcf2b0b8f6799
Sisay Chala <sisay.adugna.chala@fit.fraunhofer.de>
2019-03-27 09:04:10 +0100

    updated elements in the model

--- swagger_server/controllers/commands_controller.py
+++ swagger_server/controllers/commands_controller.py
@@ -1,260 +1,36 @@
 import connexion
 import six
-import logging, time
-#from flask import json
-import pickle
-import json
-import jsonpickle
-
 
 from swagger_server.models.simulation import Simulation  # noqa: E501
+from swagger_server.models.simulation_result import SimulationResult  # noqa: E501
 from swagger_server import util
-from data_management.controller import gridController as gControl
-from swagger_server.models.simulation_result import SimulationResult
-from swagger_server.models.voltage import Voltage
-from swagger_server.models.error import Error
-from data_management.redisDB import RedisDB
-from swagger_server.controllers.threadFactory import ThreadFactory
 
-from  more_itertools import unique_everseen
-
-
-logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s: %(message)s', level=logging.DEBUG)
-logger = logging.getLogger(__file__)
-
-
-
-class CommandController:
-
-    def __init__(self):
-        self.factory = {}
-        self.redisDB=RedisDB()
-        self.statusThread = {}
-        self.running = {}
-
-    def set_isRunning(self, id, bool):
-        self.running[id] = bool
-
-    def set(self, id, object):
-        logger.debug("Object in set: "+str(object))
-        try:
-            self.factory[id] = object
-        except Exception as e:
-            logger.debug(e)
-
-    def get(self, id):
-        return self.factory[id]
-
-    def isRunningExists(self):
-        logger.debug("IsRunning exists: " + str(len(self.running)))
-        if len(self.running):
-            return True
-        else:
-            return False
-
-    def get_isRunning(self, id):
-        if id in self.running.keys():
-            return self.running[id]
-        else:
-            return False
-
-    def get_running(self):
-        return self.running
-
-    def get_statusThread(self, id):
-        return self.statusThread[id]
-
-    def run(self, id, json_object):
-        logger.debug("Run in command controller started")
-        self.id = id
-        self.duration = json_object.duration
-        logger.debug("Duration: "+str(self.duration))
-        #gridController = gControl()
-        #self.factory= jsonpickle.decode(self.redisDB.get("factory: "+id))
-        #self.redisDB.get("factory: " + id)
-        #logger.debug("This is the factory for command controller: "+str(self.factory))
-        #self.set(self.id, self.redisDB.get("factory: " + id))
-        #gridController.setParameters(id,)
-        #logger.debug("Thread set")
-        #
-        try:
-            self.redisDB.set("run:" + self.id, "running")
-            logger.debug("Status: "+str(self.redisDB.get("run:" + self.id)))
-            logger.debug("Thread: " + str(self.get(self.id)))
-            listNames, listValues = self.get(self.id).startController(self.duration)
-            self.redisDB.set("run:" + self.id, "stop")
-            return buildAnswer(listNames, listValues, json_object.threshold_high, json_object.threshold_medium,
-                               json_object.threshold_low)
-
-        except Exception as e:
-            logger.error(e)
-            return e
-        #self.set_isRunning(self.id, True)
-        #logger.debug("Flag isRunning set to True")
-        #logger.info("running status " + str(self.running))
-
-        #logger.info("running status " + str(self.redisDB.get("run:" + self.id)))
-        #logger.debug("from redis: "+str(self.factory)+" type: "+str(type(self.factory)))
-        #self.factory=ThreadFactory()
-        #logger.debug("Normal: " + str(self.factory) + " type: " + str(type(self.factory)))
-        #listNames, listValues = self.factory.startController()
-
-        #return "started"
-        #return (listNames, listValues)
-        #return buildAnswer(listNames, listValues, json_object.threshold_high, json_object.threshold_medium, json_object.threshold_low)
-
-    def abort(self, id):
-        logger.debug("Abort signal received")
-        logger.debug("This is the factory object: " + str(self.get(id)))
-        if self.factory[id]:
-            self.factory[id].stopControllerThread()
-            self.set_isRunning(id, False)
-            message = "System stopped succesfully"
-            logger.debug(message)
-        else:
-            message = "No threads found"
-            logger.debug(message)
-
-    def run_status(self, id):
-        while True:
-            status = self.get(id).is_running()
-            flag = self.redisDB.get("run:" + id)
-            logger.debug("Control run_status: "+str(flag))
-            if not status or (flag is not None and flag == "stop"):
-                logger.debug("Control run_status: "+str(flag))
-                self.redisDB.remove("run:" + id)
-                self.stop(id)
-                break
-            time.sleep(1)
-
-variable = CommandController()
 
 def abort_simulation(id):  # noqa: E501
     """Aborts a running simulation
 
     If the user of the professional GUI decides to abort a running simulation this call will be triggered # noqa: E501
 
+    :param id: ID of the simulation that should be aborted
+    :type id: str
 
     :rtype: None
     """
-
-    try:
-        redis_db = RedisDB()
-        flag = redis_db.get("run:" + id)
-        message = ""
-        if flag is not None and flag == "running":
-            logger.debug("System running and trying to stop")
-            redis_db.set("run:" + id, "stop")
-            time.sleep(1)
-            flag = redis_db.get("run:" + id)
-            if flag is None:
-                logger.debug("System stopped succesfully")
-                message = "System stopped succesfully"
-        elif flag is None:
-            logger.debug("System already stopped")
-            message = "System already stopped"
-    except Exception as e:
-        logger.error(e)
-        message = "Error stoping the system"
-    return message
+    return 'do some magic!'
 
 
 def run_simulation(id, body):  # noqa: E501
     """Runs a simulation
 
-     # noqa: E501
+    Runs a simulation # noqa: E501
 
-    :param body: Run a simulation
+    :param id: ID of the simulation that should be started
+    :type id: str
+    :param body: Configuration data for the simulation e.g. duration
     :type body: dict | bytes
 
     :rtype: List[SimulationResult]
     """
     if connexion.request.is_json:
-        logger.info("Start command")
         body = Simulation.from_dict(connexion.request.get_json())  # noqa: E501
-
-        try:
-            redis_db = RedisDB()
-            flag = redis_db.get(id)
-            if flag is not None and flag == "created":
-                if variable.isRunningExists():
-                    logger.debug("isRunning exists")
-                    if not variable.get_isRunning(id):
-                        response = variable.run(id, body)
-                        return response
-                    else:
-                        logger.debug("System already running")
-                        return "System already running"
-                else:
-                    logger.debug("isRunning not created yet")
-                    response = variable.run(id, body)
-                    return response
-            else:
-                response = "Id not existing"
-
-        except Exception as e:
-            logger.debug("e")
-            response = e
-
-    return response
-
-
-def buildAnswer(listNames=None, listValues=None, thres_High = 0.1, thres_Medium=0.05, thres_Low=0.025):
-
-    body = []
-    values = []
-    names = []
-    values_error=[]
-
-
-    for name in listNames:
-        names.append(name.split('.',1)[0])
-        names=list(unique_everseen(names))
-
-    group_value = [None] * 3
-    for j in range(len(names)):
-        for i in range(len(listValues)):
-            if names[j] in listNames[i]:
-                if ".1"  in listNames[i]:
-                    group_value[0]=listValues[i]
-                elif ".2"  in listNames[i]:
-                    group_value[1] = listValues[i]
-                elif ".3"  in listNames[i]:
-                    group_value[2] = listValues[i]
-
-
-        errors=checkError(group_value, thres_High, thres_Medium, thres_Low)
-        values_error.append(errors)
-        voltages=Voltage(group_value[0],group_value[1],group_value[2])
-        values.append(voltages)
-        #del group_value[:]
-        group_value = [None] * 3
-
-    for i in range(len(names)):
-        body.append(SimulationResult(names[i], values[i],values_error[i]))
-
-    return body
-
-def checkError(value, thresHigh, thresMedium, thresLow):
-    group_value_error_high = [None] * 3
-    group_value_error_medium = [None] * 3
-    group_value_error_low = [None] * 3
-
-    counter = 0
-    for val in value:
-        logger.info("Counter: " + str(counter))
-        if val != None:
-            logger.info("Val: "+ str(val))
-            if float(val) < (1 - thresHigh) or float(val) > (1 + thresHigh):
-                group_value_error_high[counter] = val
-            elif (float(val) < (1 - thresMedium) or float(val) > (1 + thresMedium)):
-                group_value_error_medium[counter] = val
-            elif (float(val) < (1 - thresLow) or float(val) > (1 + thresLow)):
-                group_value_error_low[counter] = val
-
-            counter = counter + 1
-        else:
-            counter = counter + 1
-
-    error=Error(group_value_error_high,group_value_error_medium,group_value_error_low)
-    return error
+    return 'do some magic!'
--- swagger_server/controllers/simulation_controller.py
+++ swagger_server/controllers/simulation_controller.py
@@ -1,248 +1,24 @@
 import connexion
-import six, logging, os, json
-import pickle
-import jsonpickle
+import six
 
-from swagger_server.controllers.commands_controller import variable
 from swagger_server.models.grid import Grid  # noqa: E501
 from swagger_server import util
 
-from swagger_server.controllers.utils import constant as c
-from data_management.redisDB import RedisDB
-
-from swagger_server.controllers.threadFactory import ThreadFactory
-from data_management.utils import Utils
-
-logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s: %(message)s', level=logging.DEBUG)
-logger = logging.getLogger(__file__)
-
-utils = Utils()
-
-
-xycurves=[
-
-    {
-        "id":"panel_temp_eff",
-        "npts":4,
-        "xarray":[
-            0,
-            25,
-            75,
-            100
-        ],
-        "yarray":[
-            1.2,
-            1.0,
-            0.8,
-            0.6
-        ]
-    },
-    {
-        "id":"panel_absorb_eff",
-        "npts":4,
-        "xarray":[
-            0.1,
-            0.2,
-            0.4,
-            1.0
-        ],
-        "yarray":[
-            0.86,
-            0.9,
-            0.93,
-            0.97
-        ]
-    }
-
-]
-
-loadshapes = [
-
-    {
-        "id":"assumed_irrad",
-        "npts":24,
-        "interval":1,
-        "mult":[
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0.1,
-            0.2,
-            0.3,
-            0.5,
-            0.8,
-            0.9,
-            1.0,
-            1.0,
-            0.99,
-            0.9,
-            0.7,
-            0.4,
-            0.1,
-            0,
-            0,
-            0,
-            0,
-            0
-        ]
-    }
-
-]
-
-tshapes = [
-
-    {
-        "id":"assumed_Temp",
-        "npts":24,
-        "interval":1,
-        "mult":[
-            10,
-            10,
-            10,
-            10,
-            10,
-            10,
-            12,
-            15,
-            20,
-            25,
-            30,
-            30,
-            32,
-            32,
-            30,
-            28,
-            27,
-            26,
-            25,
-            20,
-            18,
-            15,
-            13,
-            12
-        ]
-    }
-
-]
 
 def create_simulation(body):  # noqa: E501
     """Send grid data to simulation engine in order to create a new simulation
 
      # noqa: E501
 
-    :param radial: Grid to be simulated
-    :type radial: dict | bytes
+    :param body: Grid to be simulated
+    :type body: dict | bytes
 
     :rtype: str
     """
     if connexion.request.is_json:
-        logger.debug("Post grid request")
-        grid = Grid.from_dict(connexion.request.get_json())  # noqa: E501
+        body = Grid.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
 
-        ####generates an id an makes a directory with the id for the data and for the registry
-        try:
-            id = utils.create_and_get_ID()
-            redis_db = RedisDB()
-            redis_db.set(id, "created")
-            flag = redis_db.get(id)
-            logger.debug("id stored in RedisDB: "+str(flag))
-            #dir = os.path.join(os.getcwd(), "utils", str(id))
-            #if not os.path.exists(dir):
-                #os.makedirs(dir)
-            #dir_data = os.path.join(os.getcwd(), "optimization", str(id), "mqtt")
-            #if not os.path.exists(dir_data):
-                #os.makedirs(dir_data)
-        except Exception as e:
-            logger.error(e)
-
-        logger.info("This is the dictionary: " + str(grid))
-        radial = grid.radials
-        logger.info("These are the radials: "+ str(radial))
-
-
-        linecodes = [c().linecodes[0]]
-        logger.debug("Linecode: "+str(linecodes))
-        #gridController= gControl()
-        factory= ThreadFactory(id)
-        variable.set(id, factory)
-        logger.debug("Factory instance stored")
-        #redis_db.set("factory: "+id, json.dumps(factory))
-        #logger.debug("Factory: "+str(factory[id]))
-        #object=redis_db.get("factory: "+id)
-        #logger.debug("Factory stored in redisDB: "+str(object))
-        #test= json.loads(object[id])
-        #logger.debug("Factory stored in redisDB: " + str(test)+" type: "+str(type(test)))
-        factory.gridController.setNewCircuit(id)
-
-        for values in radial:
-            logger.debug("values of the radial: "+str(values))
-            values=values.to_dict()
-            if "loads" in values.keys() and values["loads"] is not None:
-                # radial=radial.to_dict()
-                load = values["loads"]
-                factory.gridController.setLoads(id, load)
-            if "transformer" in values.keys() and values["transformer"] is not None:
-                transformer = values["transformer"]
-                factory.gridController.setTransformers(transformer)
-            if "powerLines" in values.keys() and values["powerLines"] is not None:
-                powerLines = values["powerLines"]
-                #linecodes = values["linecode"]
-                factory.gridController.setPowerLines(id, powerLines, linecodes) #TODO: Where does linecodes come from?
-            """and "linecode" in values.keys() and values["linecode"] is not None:"""
-
-            if "photovoltaics" in values.keys() and values["photovoltaics"] is not None:
-                photovoltaics = values["photovoltaics"]
-                # xycurves = radial["xycurves"]
-                # loadshapes = radial["loadshapes"]
-                # tshapes = radial["tshapes"]
-                factory.gridController.setPhotovoltaic(id, photovoltaics, xycurves, loadshapes, tshapes)
-
-            """
-            and "xycurves" in radial.values.keys()s() and radial["xycurves"] is not None 
-                            and "loadshapes" in radial.values.keys()s() and radial["loadshapes"] is not None 
-                            and "tshapes" in radial.values.keys()s() and radial["tshapes"] is not None: 
-            """
-            if "storageUnits" in values.keys() and values["storageUnits"] is not None:
-                # radial=radial.to_dict()
-                storage = values["storageUnits"]
-                factory.gridController.setStorage(id, storage)
-            """if "chargingPoints" in values.keys() and values["chargingPoints"] is not None:
-                # radial=radial.to_dict()
-                chargingPoints = values["chargingPoints"]
-                gridController.setChargingPoints(id, chargingPoints)
-            """
-            if "powerProfile" in values.keys() and values["powerProfile"] is not None:
-                powerProfile = values["powerProfile"]
-                factory.gridController.setPowerProfile(id, powerProfile)
-             if "chargingPoints" in values.keys() and values["chargingPoints"] is not None:
-                chargingPoints = values["chargingPoints"]
-                factory.gridController.setChargingPoints(id, chargingPoints)               
-             if "linecode" in values.keys() and values["linecode"] is not None:
-                linecode = values["linecode"]
-                factory.gridController.setLineCode(id, linecode)   
-             if "capacitor" in values.keys() and values["capacitor"] is not None:
-                capacitor = values["capacitor"]
-                factory.gridController.setCapacitor(id, capacitor) 
-             if "voltage_regulator" in values.keys() and values["voltage_regulator"] is not None:
-                voltage_regulator = values["voltage_regulator"]
-                factory.gridController.setVoltageRegulator(id, voltage_regulator) 
-             if "xycurves" in values.keys() and values["xycurves"] is not None:
-                xycurves = values["xycurves"]
-                factory.gridController.setXYCurve(id, xycurve) 
-             if "loadshapes" in values.keys() and values["loadshapes"] is not None:
-                loadshapes = values["loadshapes"]
-                factory.gridController.setLoadShape(id, loadshapes) 
-             if "tshapes" in values.keys() and values["tshapes"] is not None:
-                tshapes = values["tshapes"]
-                factory.gridController.setTShape(id, tshapes)                 
-        ######Disables circuits untilo the run simulation is started
-        factory.gridController.disableCircuit(id)
-        return str(id)
-    else:
-        return "Bad JSON Format"
 
 def delete_simulation(id):  # noqa: E501
     """Delete a simulation and its data
@@ -264,8 +40,8 @@ def update_simulation(id, body):  # noqa: E501
 
     :param id: ID of the simulation
     :type id: str
-    :param radial: Updated grid data
-    :type radial: dict | bytes
+    :param body: Updated grid data
+    :type body: dict | bytes
 
     :rtype: None
     """
