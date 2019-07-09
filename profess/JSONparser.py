65c4497f4557282852a6c7232db2ba71f37ca4e0
Robert Klingenberg <robert_klingenberg@ymail.com>
2019-07-09 13:59:56 +0200

    changed name of parser.py to JSONparser.py

--- profess/JSONparser.py
+++ profess/JSONparser.py
@@ -1,35 +1,32 @@
 import json
 from collections.abc import Iterable
 
-
-load=dict()
-nodeID="633"
 IEEE13=open("IEEE13_changed.json").read()
 
 jsonIEEE=json.loads(IEEE13)
 
-#@search returns a list with results
-#@searchIn is the dict/list we search in
-#@searchFor is the String we search for
-#@returnsAll if true, returns all where searchFor fits, even if searchID doesn't fit
+# @search returns a list with results
+    # @searchIn is the dict/list we search in
+    # @searchFor is the String we search for
+    # @returnsAll if true, returns all where searchFor fits, even if searchID doesn't fit
 def search(searchIn, searchFor, searchID, returnsAll):
-    searchResult=[]
-    dummyList=searchIn
-    if type(searchIn) ==list:
-        dummyList=[]
+    searchResult = []
+    dummyList = searchIn
+    if type(searchIn) == list:
+        dummyList = []
         for element in range(len(searchIn)):
             dummyList.append(element)
     for value in dummyList:
         if type(value) == dict or type(value) == list:
             for element in value:
-                if isinstance(value[element], Iterable) and type(value[element]) != str:
-                    searchResult = searchResult + search(value[element], searchFor, searchID, returnsAll)
+               if isinstance(value[element], Iterable) and type(value[element]) != str:
+                   searchResult = searchResult + search(value[element], searchFor, searchID, returnsAll)
         else:
             if value == searchFor:
-                if searchIn[value] == searchID or returnsAll:
-                    print("matching id")
+               if searchIn[value] == searchID or returnsAll:
+                    # print("matching id")
                     if returnsAll:
-                        if type(searchIn[value])==list:
+                        if type(searchIn[value]) == list:
                             for element in searchIn[value]:
                                 searchResult.append(element)
                         else:
@@ -37,17 +34,16 @@ def search(searchIn, searchFor, searchID, returnsAll):
                     else:
                         searchResult.append(searchIn)
             if isinstance(searchIn[value], Iterable) and type(searchIn[value]) != str:
-
                 searchResult = searchResult + search(searchIn[value], searchFor, searchID, returnsAll)
 
     return searchResult
-
 #print(search(jsonIEEE,"bus1",nodeID,False))
-#helps=[]
-#helps=search(jsonIEEE,"storageUnits","storageUnits",True)
+helps=[]
+helps=search(jsonIEEE,"storageUnits","storageUnits",True)
 #print(helps)
-#helper=search(helps,"bus1","bus1",True)
+helper=search(helps,"bus1","bus1",True)
 #print(helper)
 #print(int(helper[0]))
-#for element in range(len(helper)):
- #   print(search(jsonIEEE,"bus1",helper[element],False))
+for element in range(len(helper)):
+
+    print(search(jsonIEEE,"bus1",helper[element],False)+search(jsonIEEE,"bus",helper[element],False))
