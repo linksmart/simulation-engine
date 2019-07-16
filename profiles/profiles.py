d45aab275773b4cc15cfd80cefd285166ecc048e
Khan <saba.khan@fit.fraunhofer.de>
2019-07-16 17:25:49 +0200

    Added price, pv and load profile methods

--- profiles/profiles.py
+++ profiles/profiles.py
@@ -1,15 +1,18 @@
+"""
+Created by Saba khan: July 16 2019
+"""
+
 import datetime
-import time
 import logging
 import requests
+import time
 logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s: %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__file__)
-
 class Profiles:
     def __init__(self):
         self.url = ""
 
-    def price_calculator(self, city, country, total_days, type_location="commercial"):
+    def price_profile(self, city, country, days):
 
         base_url = "http://10.8.0.50:18081"
 
@@ -19,12 +22,12 @@ class Profiles:
 
         no_of_years = 0
         extra_days = 0
-        if(total_days > 365):
-            no_of_years = int(total_days / 365)
-            extra_days = total_days % 365
-            total_days = 365
+        if(days > 365):
+            no_of_years = int(days / 365)
+            extra_days = days % 365
+            days = 365
         start_date = datetime.datetime.now()+ datetime.timedelta(days = -365)
-        end_date = (start_date + datetime.timedelta(days=total_days)).strftime("%Y-%m-%d")
+        end_date = (start_date + datetime.timedelta(days=days)).strftime("%Y-%m-%d")
         start_date = start_date.strftime("%Y-%m-%d")
 
         if(city.lower() in ["bolzano", "fur"] and country.lower() in ["italy","denmark"]):
@@ -55,22 +58,34 @@ class Profiles:
             data_list = []
             time_series = data["Publication_MarketDocument"]["TimeSeries"]
             if isinstance(time_series, list):
-                for time_frame in time_series:
-                    for point in time_frame["Period"]["Point"]:
-                        price = round(float(point["price.amount"])/1000, 5)  # Price calculated per KWh
+                for time_frame in range(len(time_series) - 1):
+                    for point in time_series[time_frame]["Period"]["Point"]:
+                        price = float(point["price.amount"]) / 1000  # Price calculated per KWh
                         data_list.append(price)
 
             price_list = data_list.copy()
             for year in range(no_of_years-1):
                 price_list.extend(data_list)
             price_list.extend(data_list[0:extra_days*24])
-            logger.debug("Price data size = ", len(price_list))
-            logger.debug("Price data = ", price_list)
+            logger.debug("Price data size: ", len(price_list))
+            logger.debug("Price data: ", price_list)
         return price_list
 
 
-    def pv(self, lat=50.7374, lon=7.0982, days = 365):
-        rad_data = []
+    def pv_profile(self, city, country, days = 365):
+        if (not (city) or not (country)):
+            logger.error(
+                "\nPlease provide both city and country name, currenty we support price calculation for Bolzano,Italy and Fur,Denmark")
+            return None
+        lat =0
+        lon =0
+        if (city.lower() == "bolzano" and country.lower() == "italy"):
+            lat = "46.12"
+            lon = "11.12"
+
+        elif (city.lower() == "fur" and country.lower() == "denmark"):
+            lat = "56.82"
+            lon = "9.00"
         logger.info("coord " + str(lat) + ", " + str(lon))
         if lat is not None and lon is not None:
             rad = requests.get("http://re.jrc.ec.europa.eu/pvgis5/seriescalc.php?lat=" +
@@ -88,26 +103,62 @@ class Profiles:
                     break
 
                 rad.append(w)
-            rad = rad[0:days*24]
+
+            rad = rad[0:365*24]
+            no_of_years = 0
+            if (days > 365):
+                no_of_years = int(days / 365)
+                days = days % 365
             data = []
             for i in range(0, len(rad)):
-                date = rad[i][0]
-                #timestamp = date.timestamp()
                 pv_output = float(rad[i][1])
                 data.append( pv_output)
-            print("***Data length****", len(data))
-            print(data)
-            return data
+            pv_list = []
+            for i in range(no_of_years):
+                pv_list.extend(data)
+            pv_list.extend(data[0:days*24])
+            logger.debug("PV data size: "+ str(len(pv_list)))
+            logger.debug("PV data: ",pv_list)
+            return pv_list
+
+    def load_profile(self, type= "commercial", randint=0,  days=0):
+        if(randint > 50 or randint < 0):
+            logger.debug("Please provide randint values between 0-50")
+            return
+
+        if(type == "residential"):
+            file_path = "load_profiles/residential/profiles_" + str(randint) + ".txt"
+            with open(file_path, "r") as file:
+                data = file.readlines()
+                data_list = [float(i)for i in data]
+        else:
+            with open("load_profiles/commercial/commercial_general.txt", "r") as file:
+                data = file.readlines()
+                data_list = [float(i) for i in data]
+                data_list.extend(data_list[-24:])
+
+        no_of_years = 0
+        if (days > 365):
+            no_of_years = int(days / 365)
+            days = days % 365
+        load_list = []
+        for i in range(no_of_years):
+            load_list.extend(data_list)
+        load_list.extend(data_list[0:days * 24])
+        logger.debug("Load data size: ",len(load_list))
+        logger.debug("Load data: ", load_list)
 
 
-"""if __name__ == "__main__":
+if __name__ == "__main__":
     prof = Profiles()
-    t_end = time.time() + 60 * 15
-    day = 1
-    while time.time() < t_end:
-        prof.price_calculator("fur","denmark", day, "commercial")
-        day = day + 1
-        time.sleep(5)"""
 
-prof = Profiles()
-prof.pv(days=100)
+    prof.pv_profile("bolzano", "italy", days=365)
+    prof.load_profile(type="commercial", randint=0, days=365)
+
+    t_end = time.time() + 60
+    days = 1
+    while time.time() < t_end:
+        prof.price_profile("fur","denmark", days)
+        days = days + 1
+        time.sleep(5)
+
