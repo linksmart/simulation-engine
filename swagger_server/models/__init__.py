43d39490e2b1bdfff36f0a2a16197bb528a4755e
saba6099 <droptosabakhan@gmail.com>
2019-10-31 13:12:33 +0100

    Changed API responses in swagger

--- swagger_server/models/__init__.py
+++ swagger_server/models/__init__.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 # import models into model package
 from swagger_server.models.capacitor import Capacitor
 from swagger_server.models.charging_station import ChargingStation
+from swagger_server.models.currents import Currents
 from swagger_server.models.duration import Duration
 from swagger_server.models.electric_vehicle import ElectricVehicle
 from swagger_server.models.error import Error
@@ -12,15 +13,21 @@ from swagger_server.models.grid import Grid
 from swagger_server.models.grid_definition import GridDefinition
 from swagger_server.models.linecode import Linecode
 from swagger_server.models.load import Load
+from swagger_server.models.losses import Losses
 from swagger_server.models.node import Node
+from swagger_server.models.phase_val import PhaseVal
+from swagger_server.models.phase_val_max_min import PhaseValMaxMin
+from swagger_server.models.phase_value import PhaseValue
 from swagger_server.models.phases import Phases
 from swagger_server.models.photovoltaic import Photovoltaic
 from swagger_server.models.power_profile import PowerProfile
 from swagger_server.models.powerline import Powerline
+from swagger_server.models.powers import Powers
 from swagger_server.models.radial import Radial
 from swagger_server.models.reg_control import RegControl
 from swagger_server.models.simulation import Simulation
 from swagger_server.models.simulation_result import SimulationResult
+from swagger_server.models.simulation_result2 import SimulationResult2
 from swagger_server.models.simulation_result_raw import SimulationResultRaw
 from swagger_server.models.storage import Storage
 from swagger_server.models.transformer import Transformer
@@ -28,5 +35,6 @@ from swagger_server.models.transformer_regcontrol import TransformerRegcontrol
 from swagger_server.models.transformer_wdgs import TransformerWdgs
 from swagger_server.models.tshape import Tshape
 from swagger_server.models.voltage import Voltage
+from swagger_server.models.voltages import Voltages
 from swagger_server.models.winding import Winding
 from swagger_server.models.xy_curve import XYCurve
--- swagger_server/models/simulation_result.py
+++ swagger_server/models/simulation_result.py
@@ -4,7 +4,10 @@ from __future__ import absolute_import
 from datetime import date, datetime  # noqa: F401
 
 from typing import List, Dict  # noqa: F401
-
+from swagger_server.models.currents import Currents
+from swagger_server.models.losses import Losses
+from swagger_server.models.powers import Powers
+from swagger_server.models.voltages import Voltages
 from swagger_server.models.base_model_ import Model
 from swagger_server.models.error import Error  # noqa: F401,E501
 from swagger_server.models.voltage import Voltage  # noqa: F401,E501
@@ -17,31 +20,36 @@ class SimulationResult(Model):
     Do not edit the class manually.
     """
 
-    def __init__(self, node_id: str=None, voltage: Voltage=None, error: Error=None):  # noqa: E501
+    def __init__(self, currents: Currents=None, losses: Losses=None, powers: Powers=None, voltages: Voltages=None):  # noqa: E501
         """SimulationResult - a model defined in Swagger
 
-        :param node_id: The node_id of this SimulationResult.  # noqa: E501
-        :type node_id: str
-        :param voltage: The voltage of this SimulationResult.  # noqa: E501
-        :type voltage: Voltage
-        :param error: The error of this SimulationResult.  # noqa: E501
-        :type error: Error
+        :param currents: The currents of this SimulationResult.  # noqa: E501
+        :type currents: Currents
+        :param losses: The losses of this SimulationResult.  # noqa: E501
+        :type losses: Losses
+        :param powers: The powers of this SimulationResult.  # noqa: E501
+        :type powers: Powers
+        :param voltages: The voltages of this SimulationResult.  # noqa: E501
+        :type voltages: Voltages
         """
         self.swagger_types = {
-            'node_id': str,
-            'voltage': Voltage,
-            'error': Error
+            'currents': Currents,
+            'losses': Losses,
+            'powers': Powers,
+            'voltages': Voltages
         }
 
         self.attribute_map = {
-            'node_id': 'nodeId',
-            'voltage': 'voltage',
-            'error': 'error'
+            'currents': 'currents',
+            'losses': 'losses',
+            'powers': 'powers',
+            'voltages': 'voltages'
         }
 
-        self._node_id = node_id
-        self._voltage = voltage
-        self._error = error
+        self._currents = currents
+        self._losses = losses
+        self._powers = powers
+        self._voltages = voltages
 
     @classmethod
     def from_dict(cls, dikt) -> 'SimulationResult':
@@ -55,64 +63,85 @@ class SimulationResult(Model):
         return util.deserialize_model(dikt, cls)
 
     @property
-    def node_id(self) -> str:
-        """Gets the node_id of this SimulationResult.
+    def currents(self) -> Currents:
+        """Gets the currents of this SimulationResult.
 
 
-        :return: The node_id of this SimulationResult.
-        :rtype: str
+        :return: The currents of this SimulationResult.
+        :rtype: Currents
         """
-        return self._node_id
+        return self._currents
 
-    @node_id.setter
-    def node_id(self, node_id: str):
-        """Sets the node_id of this SimulationResult.
+    @currents.setter
+    def currents(self, currents: Currents):
+        """Sets the currents of this SimulationResult.
 
 
-        :param node_id: The node_id of this SimulationResult.
-        :type node_id: str
+        :param currents: The currents of this SimulationResult.
+        :type currents: Currents
         """
 
-        self._node_id = node_id
+        self._currents = currents
 
     @property
-    def voltage(self) -> Voltage:
-        """Gets the voltage of this SimulationResult.
+    def losses(self) -> Losses:
+        """Gets the losses of this SimulationResult.
 
 
-        :return: The voltage of this SimulationResult.
-        :rtype: Voltage
+        :return: The losses of this SimulationResult.
+        :rtype: Losses
         """
-        return self._voltage
+        return self._losses
 
-    @voltage.setter
-    def voltage(self, voltage: Voltage):
-        """Sets the voltage of this SimulationResult.
+    @losses.setter
+    def losses(self, losses: Losses):
+        """Sets the losses of this SimulationResult.
 
 
-        :param voltage: The voltage of this SimulationResult.
-        :type voltage: Voltage
+        :param losses: The losses of this SimulationResult.
+        :type losses: Losses
         """
 
-        self._voltage = voltage
+        self._losses = losses
 
     @property
-    def error(self) -> Error:
-        """Gets the error of this SimulationResult.
+    def powers(self) -> Powers:
+        """Gets the powers of this SimulationResult.
 
 
-        :return: The error of this SimulationResult.
-        :rtype: Error
+        :return: The powers of this SimulationResult.
+        :rtype: Powers
         """
-        return self._error
+        return self._powers
 
-    @error.setter
-    def error(self, error: Error):
-        """Sets the error of this SimulationResult.
+    @powers.setter
+    def powers(self, powers: Powers):
+        """Sets the powers of this SimulationResult.
 
 
-        :param error: The error of this SimulationResult.
-        :type error: Error
+        :param powers: The powers of this SimulationResult.
+        :type powers: Powers
         """
 
-        self._error = error
+        self._powers = powers
+
+    @property
+    def voltages(self) -> Voltages:
+        """Gets the voltages of this SimulationResult.
+
+
+        :return: The voltages of this SimulationResult.
+        :rtype: Voltages
+        """
+        return self._voltages
+
+    @voltages.setter
+    def voltages(self, voltages: Voltages):
+        """Sets the voltages of this SimulationResult.
+
+
+        :param voltages: The voltages of this SimulationResult.
+        :type voltages: Voltages
+        """
+
+        self._voltages = voltages
