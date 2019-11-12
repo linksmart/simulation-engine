21a88487d6ac9aaafb4a41a5187b960b858df6df
saba6099 <droptosabakhan@gmail.com>
2019-11-12 14:59:24 +0100

    Supports transformers and linecode parsing

--- tests/data/13Bus/dss_to_json.py
+++ tests/data/13Bus/dss_to_json.py
@@ -7,6 +7,7 @@ class dssToJson:
 		previous_type = ''
 		result['radials'] = {}
 		result['radials']['transformers'] = []
+		result['radials']['linecode'] = []
 		with open('IEEE13Nodeckt.dss', 'r') as f:
 			for cnt, line in enumerate(f):
 				words = line.strip().split(" ")
@@ -66,7 +67,45 @@ class dssToJson:
 						result['radials']['transformers'].append(trans)
 						previous_type = 'Transformer'
 						previous_index = len(result['radials']['transformers'])
-				
+					if(type == "linecode"):
+						# linecode = []
+						linecode = {}
+						linecode['id'] = words[1].split('.')[1]
+						for word in words[1:]:
+							if word != ' ' and word != '~' and word != '':
+								split_word = word.split('=')
+								if (len(split_word) > 1):
+									linecode[split_word[0]] = split_word[1]
+						previous_type = "linecode"
+						result['radials']['linecode'].append(linecode)
+				if (words[0] == '~' and previous_type == 'linecode'):
+					word_set = ""
+					previous_set = False
+					val = []
+					for index in range(len(words)):
+						word = words[index]
+						# operation_word = ''
+						# value_set = False
+						split_word = word.split('=')
+						key = ""
+						# line_dict = {}
+						if(word == "rmatrix" or word == "xmatrix"):
+							linecode[word] = []
+							word_set = word
+						if (len(split_word) > 1 and "" not in split_word):
+							key = split_word[0]
+							linecode[key] = split_word[1]
+						if "(" in word or previous_set:
+							previous_set = True
+							if word == "|":
+								linecode[word_set].append(val)
+								val = []
+							if(word == ')'):
+								linecode[word_set].append(val)
+								previous_set = False
+							if(not word == '|' and not word == ')'):
+								val.append(float(re.findall(r'(?:(?:\d*\.\d+)|(?:\d+\.?))', word)[0]))
+						
 				if(words[0]=='~' and previous_type == 'circuit'):
 					for word in words[1:]:
 						if(word.split('=')[0] == 'pu'):
@@ -86,22 +125,22 @@ class dssToJson:
 							if (len(split_word) > 1):
 								operation_word = split_word[1]
 							if "(" in operation_word:
-								first = float(re.findall(r'\d+', operation_word)[0])
+								first = float(re.findall(r'(?:(?:\d*\.\d+)|(?:\d+\.?))', operation_word)[0])
 								# while ")" not in operation_word:
 								second_word = words[index + 1]
-								second = int(re.findall(r'\d+', second_word)[0])
+								second = float(re.findall(r'(?:(?:\d*\.\d+)|(?:\d+\.?))', second_word)[0])
 								index = index + 1
 								value = first/second
 								value_set = True
 							if '[' in operation_word:
 								elements = []
 								operation_word = split_word[1]
-								elements.append((re.findall(r'\d+', operation_word)[0]))
+								elements.append(float(re.findall(r'(?:(?:\d*\.\d+)|(?:\d+\.?))', operation_word)[0]))
 								second_word = ''
 								while ']' not in operation_word:
 									operation_word = words[index + 1]
 									if operation_word != '':
-										elements.append(re.findall(r'\d+', operation_word)[0])
+										elements.append(float(re.findall(r'(?:(?:\d*\.\d+)|(?:\d+\.?))', operation_word)[0]))
 									index = index + 1
 								value_set = True
 								value = elements
@@ -119,6 +158,7 @@ class dssToJson:
 					# previous_index = previous_index + 1
 				cnt = len(words)
 		print("***Result***")
+		# print(json.dumps(result))
 		print(result)
 ob = dssToJson()
 ob.convert()
